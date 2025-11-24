3cd3ad11a57c1c61f50b2d79691f14af832310b8
zerolpls <2111086@mail.nankai.edu.cn>
2025-11-24 20:34:41 +0800

    minor

--- shells/train_musa_task4.sh
+++ shells/train_musa_task4.sh
@@ -1,2 +1,2 @@
 #task4
-python train.py --device musa:3 --T 48 --max_len 49 --task_type 3 --vocab_size 8909 --batch_size 64 --epochs 100 --learning_rate 0.005 --n_embd 16 --n_hidden 8 --n_layer 4 --dropout 0.0 --model_save_path weights/jinan/task4/ --trajs_path data/jinan/task4_data/
+python train.py --device musa:3 --T 48 --max_len 49 --task_type 3 --vocab_size 8909 --batch_size 64 --epochs 100 --learning_rate 0.005 --n_embd 16 --n_hidden 8 --n_layer 32 --dropout 0.0 --model_save_path weights/jinan/task4/ --trajs_path data/jinan/task4_data/
